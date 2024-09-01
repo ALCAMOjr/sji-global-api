88ef8e0b8f6098af463bbf0bca57ae08c8eb1aa8
Alfredo <alfredocastellanoula.14@gmail.com>
2024-09-01 10:31:38 -0600

    Scraping

--- src/helpers/expedienteWorker.js
+++ src/helpers/expedienteWorker.js
@@ -25,59 +25,70 @@ dotenv.config();
 
 
 
-expedienteQueue.process(5, async (job) => {
-    let browser, page;
-    let expedientesConDetalles = [];
-    let processedCount = 0;
-
-    try {
-        const expedientes = await ExpedienteDAO.findAll();
-        for (const expediente of expedientes) {
-            const { numero, url } = expediente;
-            if (url) {
-                try {
-                    ({ browser, page } = await initializeBrowser());
-                    const scrapedData = await fillExpTribunalA(page, url);
-                    const { juzgado = '', juicio = '', ubicacion = '', partes = '', expediente: scrapedExpediente = '' } = scrapedData;
-
-                    await ExpedienteDAO.update(new Expediente(numero, expediente.nombre, url, scrapedExpediente, juzgado, juicio, ubicacion, partes));
-                    await ExpedienteDetalleDAO.deleteByExpTribunalANumero(numero);
-
-                    const scrapedDetails = await scrappingDet(page, url);
-                    if (scrapedDetails.length > 0) {
-                        for (const detail of scrapedDetails) {
-                            const expedienteDetalle = new ExpedienteDetalle(null, detail.verAcuerdo, detail.fecha, detail.etapa, detail.termino, detail.notificacion, scrapedExpediente, numero);
-                            await ExpedienteDetalleDAO.create(expedienteDetalle);
+    expedienteQueue.process(5, async (job) => {
+        let browser, page;
+        let expedientesConDetalles = [];
+        let processedCount = 0;
+    
+        try {
+            const expedientes = await ExpedienteDAO.findAll();
+            for (const expediente of expedientes) {
+                const { numero, url } = expediente;
+                if (url) {
+                    try {
+                        ({ browser, page } = await initializeBrowser());
+                        
+                        try {
+                            // Intentamos hacer el scraping
+                            const scrapedData = await fillExpTribunalA(page, url);
+                            const { juzgado = '', juicio = '', ubicacion = '', partes = '', expediente: scrapedExpediente = '' } = scrapedData;
+    
+                            await ExpedienteDAO.update(new Expediente(numero, expediente.nombre, url, scrapedExpediente, juzgado, juicio, ubicacion, partes));
+                            await ExpedienteDetalleDAO.deleteByExpTribunalANumero(numero);
+    
+                            const scrapedDetails = await scrappingDet(page, url);
+                            if (scrapedDetails.length > 0) {
+                                for (const detail of scrapedDetails) {
+                                    const expedienteDetalle = new ExpedienteDetalle(null, detail.verAcuerdo, detail.fecha, detail.etapa, detail.termino, detail.notificacion, scrapedExpediente, numero);
+                                    await ExpedienteDetalleDAO.create(expedienteDetalle);
+                                }
+                            }
+    
+                        } catch (error) {
+                            console.error(`Error scraping expediente con número ${numero}:`, error);
+                            continue;
+                        } finally {
+                            if (browser) await browser.close();
                         }
+                        
+                        processedCount++;
+                        const progress = Math.round((processedCount / expedientes.length) * 100);
+                        console.log("Progreso en iteración", progress);
+                        job.progress(progress);
+                        
+                    } catch (error) {
+                        console.error(`Error al inicializar el navegador para el expediente con número ${numero}:`, error);
+                        continue;
                     }
-
-                } finally {
-                    if (browser) await browser.close();
                 }
-                processedCount++;
-                
-                const progress = Math.round((processedCount / expedientes.length) * 100);
-
-                console.log("Progeso en iteracion", progress)
-                job.progress(progress);
             }
+    
+            const updatedExpedientes = await ExpedienteDAO.findAll();
+            for (const expediente of updatedExpedientes) {
+                const detalles = await ExpedienteDetalleDAO.findByExpTribunalANumero(expediente.numero);
+                expedientesConDetalles.push({
+                    ...expediente,
+                    detalles
+                });
+            }
+            return expedientesConDetalles;
+    
+        } catch (error) {
+            console.error('Error during the expediente update process:', error);
+            throw new Error('Error during the expediente update process');
         }
-
-        const updatedExpedientes = await ExpedienteDAO.findAll();
-        for (const expediente of updatedExpedientes) {
-            const detalles = await ExpedienteDetalleDAO.findByExpTribunalANumero(expediente.numero);
-            expedientesConDetalles.push({
-                ...expediente,
-                detalles
-            });
-        }
-        return expedientesConDetalles;
-
-    } catch (error) {
-        console.error('Error during the expediente update process:', error);
-        throw new Error('Error during the expediente update process');
-    }
-});
+    });
+    
 
 
 export const cleanJobs = async (states) => {
