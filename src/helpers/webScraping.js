3f98e10a5f31b9d9efcc7eaa90e44bda0e695c01
Alfredo <alfredocastellanoula.14@gmail.com>
2024-09-22 17:35:57 -0600

    Catchap

--- src/helpers/webScraping.js
+++ src/helpers/webScraping.js
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
