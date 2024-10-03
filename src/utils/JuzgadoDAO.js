b7050f0b77f3bab3ede55e1a0f89d50b5dff270c
Alfredo <alfredocastellanoula.14@gmail.com>
2024-10-03 15:26:44 -0600

    Filtros

--- src/utils/JuzgadoDAO.js
+++ src/utils/JuzgadoDAO.js
@@ -2,7 +2,7 @@ import { pool } from "../db.js";
 
 class JuzgadoDao {
     static async getAll() {
-        const [rows] = await pool.query('SELECT juspos FROM juzgados');
+        const [rows] = await pool.query('SELECT juspos, abreviaciones FROM juzgados');
         return rows;
     }
 }
